5186655ace8b45df7ca122c7b0b0f711f394f79d
Saurabh <sauravvishwakarma030@gmail.com>
2025-04-11 12:19:38 +0530

    Revised the frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <span>saurabh</span>
+    </>
+  );
+}
+
+export default App;
--- client/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    content: [
-        './client/src/**/*.{js,jsx,ts,tsx}', // Adjust the path according to your project structure
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
